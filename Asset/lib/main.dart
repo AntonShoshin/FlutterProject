2962f5b04cc41a6dd044c3433df604915062707f
ShoshinA <antonshoshin2@gmail.com>
2023-09-19 08:59:32 +0400

    Master

--- Asset/lib/main.dart
+++ Asset/lib/main.dart
@@ -14,99 +14,14 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.blueAccent,
         appBar: AppBar(
           backgroundColor: Colors.indigo,
-          title: const Text('Counter'),
+          title: const Text(''),
           centerTitle: true,
         ),
         body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text(
-                'Tap "-" to decrement',
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 5),
-                child: Container(
-                    constraints:
-                        const BoxConstraints.expand(width: 130, height: 50),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: Colors.white54,
-                    ),
-                    child: const CounterWidget()),
-              ),
-              const Text(
-                'Tap "-" to increment',
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                ),
-              ),
-            ],
-          ),
+          child: null,
         ),
       ),
     );
   }
 }
 
-class CounterWidget extends StatefulWidget {
-  const CounterWidget({super.key});
-
-  @override
-  State<CounterWidget> createState() => _CounterWidgetState();
-}
-
-class _CounterWidgetState extends State<CounterWidget> {
-  late int _count;
-
-  @override
-  void initState() {
-    _count = 50;
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        IconButton(
-          onPressed: () {
-            _add(-1);
-          },
-          icon: const Icon(
-            Icons.remove,
-            color: Colors.black,
-          ),
-        ),
-        Text(
-          '$_count',
-          style: const TextStyle(
-            fontSize: 20,
-          ),
-        ),
-        IconButton(
-          onPressed: () {
-            _add(1);
-          },
-          icon: const Icon(
-            Icons.add,
-            color: Colors.black,
-          ),
-        ),
-      ],
-    );
-  }
-
-  void _add(int x) {
-    _count += x;
-    setState(() {});
-  }
-}
