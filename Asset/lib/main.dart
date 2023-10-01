7a2abdcce7b210298de7b167d48e8705c82a4e3e
ShoshinA <antonshoshin2@gmail.com>
2023-10-02 00:59:53 +0400

    Master

--- Asset/lib/main.dart
+++ Asset/lib/main.dart
@@ -27,8 +27,10 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        titleTextStyle:
-            TextStyle(color: Theme.of(context).primaryColor, fontSize: 20),
+        titleTextStyle: TextStyle(
+            color: Theme.of(context).primaryColor,
+            fontSize: 20,
+            fontWeight: FontWeight.bold),
         title: const Text('Weather Forecast'),
         centerTitle: true,
         elevation: 0,
@@ -48,6 +50,10 @@ class BodyWF extends StatelessWidget {
       child: Column(
         children: [
           Search(),
+          CityDetails(),
+          TemperatureDeatil(),
+          ExtraWeatherDetail(),
+          BottomDetail(),
         ],
       ),
     );
@@ -70,5 +76,148 @@ class Search extends StatelessWidget {
         border: InputBorder.none,
       ),
     );
+  }
+}
+
+class CityDetails extends StatelessWidget {
+  const CityDetails({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(
+          'Murmansk Oblast, RU',
+          style: TextStyle(
+            color: Theme.of(context).primaryColor,
+            fontSize: 30,
+          ),
+        ),
+        Text(
+          'Friday, Mar 20, 2020',
+          style: TextStyle(
+            color: Theme.of(context).primaryColor,
+            fontSize: 18,
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class TemperatureDeatil extends StatelessWidget {
+  const TemperatureDeatil({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(
+            Icons.sunny,
+            color: Theme.of(context).primaryColor,
+            size: 70,
+          ),
+          const SizedBox(width: 10),
+          Column(
+            children: [
+              Text(
+                '14 °F',
+                style: TextStyle(
+                    color: Theme.of(context).primaryColor, fontSize: 50),
+              ),
+              const SizedBox(height: 5),
+              Text(
+                'LIGHT SNOW',
+                style: TextStyle(color: Theme.of(context).primaryColor),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class ExtraWeatherDetail extends StatelessWidget {
+  const ExtraWeatherDetail({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        WeatherDetailItem(
+          text1: '5',
+          text2: 'km/hr',
+        ),
+        WeatherDetailItem(
+          text1: '3',
+          text2: '%',
+        ),
+        WeatherDetailItem(
+          text1: '20',
+          text2: '%',
+        ),
+      ],
+    );
+  }
+}
+
+class WeatherDetailItem extends StatelessWidget {
+  String text1;
+  String text2;
+
+  WeatherDetailItem({super.key, required this.text1, required this.text2});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Icon(
+          Icons.ac_unit,
+          color: Theme.of(context).primaryColor,
+          size: 30,
+        ),
+        Text(
+          text1,
+          style: TextStyle(color: Theme.of(context).primaryColor, fontSize: 20),
+        ),
+        Text(
+          text2,
+          style: TextStyle(color: Theme.of(context).primaryColor),
+        ),
+      ],
+    );
+  }
+}
+
+class BottomDetail extends StatelessWidget {
+  const BottomDetail({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    List<String> days = <String>[
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+      "Sunday"
+    ];
+
+    return ListView.separated(
+      scrollDirection: Axis.horizontal,
+        itemBuilder: (BuildContext context, int index) {
+          return Container(
+              padding: EdgeInsets.symmetric(vertical: 10),
+              child: Text(days[index], style: TextStyle(fontSize: 22))
+          );
+        },
+        separatorBuilder: (BuildContext context, int index) =>
+            const SizedBox(width: 10),
+        itemCount: days.length);
   }
 }
