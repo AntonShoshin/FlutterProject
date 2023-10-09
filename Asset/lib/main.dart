9a19da4883b48f669d08419d9abcd463aa0fb6eb
ShoshinA <antonshoshin2@gmail.com>
2023-10-09 23:31:28 +0400

    easy_navigation

--- Asset/lib/main.dart
+++ Asset/lib/main.dart
@@ -1,27 +1,72 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MaterialApp(
+    home: const FirstHome(),
+    routes: {
+      FirstHome.routeName: (context) => FirstHome(),
+      SecondHome.routeName: (context) => SecondHome(),
+    },
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class FirstHome extends StatelessWidget {
+  const FirstHome({super.key});
+  static const routeName = '/first';
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.blueAccent,
-        appBar: AppBar(
-          backgroundColor: Colors.indigo,
-          title: const Text(''),
-          centerTitle: true,
-        ),
-        body: Center(
-          child: null,
+    return Scaffold(
+      backgroundColor: Colors.blueAccent,
+      appBar: AppBar(
+        backgroundColor: Colors.indigo,
+        title: const Text('First Home'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pushNamed(context, SecondHome.routeName, arguments: User('Anton', 34));
+          },
+          child: const Text('Second Home'),
         ),
       ),
     );
   }
 }
 
+class SecondHome extends StatelessWidget {
+  late User user;
+  static const routeName = '/second';
+
+  SecondHome({super.key});
+  @override
+  Widget build(BuildContext context) {
+    RouteSettings settings = ModalRoute.of(context)!.settings;
+    user = settings.arguments as User;
+
+    return Scaffold(
+      backgroundColor: Colors.blueAccent,
+      appBar: AppBar(
+        backgroundColor: Colors.indigo,
+        title: Text('${user.name} - ${user.age}'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: const Text('First Home'),
+        ),
+      ),
+    );
+  }
+}
+
+class User{
+  final String name;
+  final int age;
+
+  User(this.name, this.age);
+}
