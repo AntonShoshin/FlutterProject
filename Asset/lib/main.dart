db598b5dfeca31f215a0a490e8ea54728cb19ba7
ShoshinA <antonshoshin2@gmail.com>
2023-11-08 23:57:35 +0400

    form_example

--- Asset/lib/main.dart
+++ Asset/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MyApp());
@@ -10,18 +11,61 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.blueAccent,
-        appBar: AppBar(
-          backgroundColor: Colors.indigo,
-          title: const Text(''),
-          centerTitle: true,
-        ),
-        body: Center(
-          child: null,
-        ),
+      title: 'Networking',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({
+    super.key,
+  });
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.indigo,
+        title: const Text('Networking'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: null,
       ),
     );
   }
 }
 
+Future<http.Response> getData() async {
+
+  Uri url = Uri.https('about.google', 'static/data/locations.json');
+  return await http.get(url);
+
+}
+
+void loadData() {
+  getData().then(
+    (response) {
+      if (response.statusCode == 200) {
+        print(response.body);
+      } else {
+        print(response.statusCode);
+      }
+    },
+  ).catchError((error) {
+    debugPrint(error.toString());
+  });
+}
